192acaff6670e2591a96d36b3288a621b31f8004
NullOne01 <NullOne01@users.noreply.github.com>
2022-10-14 20:33:10 +0300

    Added ScanResult.cs

---
+++ scan_util/Data/ScanResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace scan_util.Data
+{
+    public class ScanResult
+    {
+        public long ExecutionTime { private get; set; } = 0;
+        public int ErrorsNum { get; set; } = 0;
+        public int FileProcessedNum { get; set; } = 0;
+
+        private readonly Dictionary<string, int> _malwareCounter = new Dictionary<string, int>();
+
+        public void LoadMalwareName(string malwareName)
+        {
+            _malwareCounter[malwareName]++;
+        }
+
+        public override string ToString()
+        {
+            // Processed files: 150
+            // JS detects: 5
+            // rm -rf detects: 1
+            // Rundll32 detects: 2
+            // Errors: 1
+            // Execution time: 00:00:31
+
+            TimeSpan timeSpan = TimeSpan.FromMilliseconds(ExecutionTime);
+            
+            var answerStr = $"Processed files: {FileProcessedNum}\n" +
+                            $"JS detects: {_malwareCounter.GetValueOrDefault("MALWARE_JS")}\n" +
+                            $"rm -rf detects: {_malwareCounter.GetValueOrDefault("MALWARE_RM_RF")}\n" +
+                            $"Rundll32 detects: {_malwareCounter.GetValueOrDefault("MALWARE_SUS")}\n" +
+                            $"Errors: {ErrorsNum}\n" +
+                            $"Execution time: {timeSpan}";
+
+            return answerStr;
+        }
+    }
+}
