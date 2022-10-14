fcde078a38ef82b6e8328be96987fab6a27b571c
NullOne01 <NullOne01@users.noreply.github.com>
2022-10-14 20:33:01 +0300

    Added class for user's input

---
+++ scan_util/Input/ArgumentReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace scan_util.Input
+{
+    public class ArgumentReader
+    {
+        public DirectoryInfo ReadDirectoryInfo(string[] args, int index)
+        {
+            if (args.Length <= index)
+            {
+                throw new ArgumentException("How to use: scan_util PATH_TO_DIRECTORY");
+            }
+
+            string path = args[index];
+            // Load environment variables.
+            path = Environment.ExpandEnvironmentVariables(path);
+            DirectoryInfo directory = new DirectoryInfo(path);
+            if (!directory.Exists)
+            {
+                throw new ArgumentException("Directory cannot be found");
+            }
+
+            return directory;
+        }
+    }
+}
