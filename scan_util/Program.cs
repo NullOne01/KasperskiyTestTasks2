2bada4862f9e01d25bfd5b5065ce78b912eddae6
NullOne01 <NullOne01@users.noreply.github.com>
2022-10-14 20:33:23 +0300

    Now app works like need

--- scan_util/Program.cs
+++ scan_util/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.IO;
+using scan_util.Input;
+using scan_util.Scanner;
 
 namespace scan_util
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArgumentReader argumentReader = new ArgumentReader();
+            DirectoryInfo directory = argumentReader.ReadDirectoryInfo(args, 0);
+
+            MalwareScanner scanner = new MalwareScanner();
+            var scannerResult = scanner.ScanDirectory(directory);
+            
+            Console.WriteLine("====== Scan result ======");
+            Console.WriteLine(scannerResult);
+            Console.WriteLine("=========================");
         }
     }
 }
